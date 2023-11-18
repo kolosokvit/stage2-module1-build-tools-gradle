685cb3ad086cf7e10e7a34d828704e5a9c245f9d
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-18 22:57:35 +0300

    Update logic

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -4,10 +4,17 @@ public class StringUtils {
     public static boolean isPositiveNumber(String str) {
         try {
             double number = Double.parseDouble(str);
+            if (str.startsWith("0") && number >= 1) {
+                return false;
+            }
             return number > 0;
         } catch (NumberFormatException | NullPointerException e) {
             e.printStackTrace();
         }
         return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPositiveNumber("088"));
     }
 }
