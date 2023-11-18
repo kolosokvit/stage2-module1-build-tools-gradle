438c5ae8e95a12746bd5c1e052cbed4207e91280
Vitali Kalaskou <kolosokvit@gmail.com>
2023-11-18 22:34:23 +0300

    First commit

--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -2,6 +2,12 @@ package com.epam.utils;
 
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-         //here magic will happen
+        try {
+            double number = Double.parseDouble(str);
+            return number > 0;
+        } catch (NumberFormatException | NullPointerException e) {
+            e.printStackTrace();
+        }
+        return false;
     }
 }
